be428422eba7cf059f8f49d455897bbea988853e
panda2134 <smallpanda2134@gmail.com>
2015-08-15 19:27:54 +0800

    VERSION 1.0!!!!!!
    i18n OK
    but now only English

---
+++ Updater/lang/en_US.py
@@ -0,0 +1,23 @@
+lang={
+        "font":"Helvetica",
+        "fontSerif":"Times New Roman",
+        "info":"Info",
+        "error":"Error",
+        "localVerError":"Local Version Error:\nEdit the config or\nupdate the modpack may help.",
+        "configNeedsCorrect":"Please edit your config file correctly.",
+        "remoteVerGetError":"Failed to get the remote version.",
+        "updatingInfo":"Updating...\nDownloaded%d%%",
+        "downloadFailed":"Download Failed.",
+        "unzipping":"Downloaded Successfully,Uncompressing...",
+        "unzipFailed":"Uncompress Failed.",
+        "updatedSuccessfully":"Updated Successfully.\nVersion : %.2f\n%s",
+        "chkUpdate":"Check for Update",
+        "updateFound":"A newer version of the modpack was found",
+        "updateFoundTip":"Would you like to update to that version?",
+        "noUpdate":"No newer version of your modpack",
+        "noUpdateTip":"The local version of your modpack is the newest.\nGo And Play!",
+        "title":"Modpack Updater",
+        "verInfo":"Version : %.2f\n%s",
+        "exitSuccessfully":"Exited Successfully",
+}
+        
